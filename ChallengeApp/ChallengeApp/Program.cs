6c020e37225bc8689207c2d403bc5a43e4b34e5b
Mateusz Przybyła <mateusz.piotr.przybyla@gmail.com>
2023-04-19 18:24:17 +0200

    Zadanie Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string name = "Ewa";
+string sex = "Kobieta";
+int age = 33;
+
+if (sex == "Kobieta")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age >= 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (age >= 18)
+{
+    Console.WriteLine("Pełnoletni mężczyzna");
+}
