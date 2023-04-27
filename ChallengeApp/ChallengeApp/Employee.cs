161098443ce352c05fb5eebdfb3a63d94ae0e75d
Mateusz Przybyła <mateusz.piotr.przybyla@gmail.com>
2023-04-27 07:22:28 +0200

    Zadanie Dzień 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,29 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> scores = new List<int>();
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.scores.Sum();
+            }
+        }
+        public void AddScore(int number)
+        {
+            this.scores.Add(number);
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,9 +1,77 @@
-﻿// Napisz program w C#, który w pętli wyświetli na konsoli cały alfabet. 
+﻿// 1. Stwórz klasę Employee, w której przechowasz imię, nazwisko, wiek 
+// oraz punkty pracownika w postaci liczb całkowitych.
+// 2. Stwórz 3 pracowników i każdemu przydziel po 5 ocen z zakresu od 1 do 10.
+// 3. Napisz program, który wyszuka pracownika z najwyższą liczbą ocen, 
+// a następnie wyświetli jego dane oraz wyniki. 
 
-char startletter = 'A';
-char endletter = 'Z';
 
-for (char i=startletter; i<=endletter; i++)
+using ChallengeApp;
+
+Employee employee1 = new Employee("Adam", "Nowak", 25);            //20
+Employee employee2 = new Employee("Monika", "Kowalska", 20);       //25
+Employee employee3 = new Employee("Kasia", "Mickiewicz", 35);      //15
+
+employee1.AddScore(0);
+employee1.AddScore(2);
+employee1.AddScore(4);
+employee1.AddScore(6);
+employee1.AddScore(8);
+
+employee2.AddScore(1);
+employee2.AddScore(3);
+employee2.AddScore(5);
+employee2.AddScore(7);
+employee2.AddScore(9);
+
+employee3.AddScore(1);
+employee3.AddScore(2);
+employee3.AddScore(3);
+employee3.AddScore(4);
+employee3.AddScore(5);
+
+
+List<Employee> employees = new List<Employee>()
 {
-    Console.WriteLine(i);
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach(var employee in employees)
+{
+    if(employee1.Result > maxResult)
+    {
+        employeeWithMaxResult = employee1;
+        maxResult = employee1.Result;
+    }
+    else if (employee2.Result > maxResult)
+    {
+        employeeWithMaxResult = employee2;
+        maxResult = employee2.Result;
+    }
+    else if (employee3.Result > maxResult)
+    {
+        employeeWithMaxResult = employee3;
+        maxResult = employee3.Result;
+    }
 }
+
+Console.WriteLine("Pracownik z najwyższą liczbą ocen: " + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname);
+Console.WriteLine("Wiek: " + employeeWithMaxResult.Age + " " + "lat");
+Console.WriteLine("Wynik: " + employeeWithMaxResult.Result);
+
+
+
+
+
+
+// User (typ) user1 (użytkownik1) = new User();
+// modyfikator dostępu: public, private.
+// konstruktor: dodatkowe miejsce, które jest wywoływane w momencie gdy tworzony jest dany obiekt. 
+// W konstruktorze możemy wpisać dane prywatne danych użytkowników danej klasy. 
+
+// nazwa klasy: User
+// pola: parametry opisujące nasz obiekt.
+// metoda: fragment kodu, który można sobie uruchomić wewnątrz lub na zewnątrz klasy,
+// metody mogą być publiczne lub prywatne.
