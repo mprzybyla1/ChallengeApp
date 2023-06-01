d807e84cecaec8cc493415c40c0e0bbec03c3f49
Mateusz Przybyła <mateusz.piotr.przybyla@gmail.com>
2023-06-01 07:22:52 +0200

    Zadanie Dzień 15

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,21 @@
 ﻿using ChallengeApp;
 
-Console.WriteLine("Witamy w Programie XYZ do oceny Pracowników");
-Console.WriteLine("===========================================");
+Console.WriteLine("Welcome to XYZ Program to collect grades of your Employees and Supervisors and finally get statistics");
+Console.WriteLine("======================================================");
 Console.WriteLine();
 
-var employee = new Employee();
+var employee = new Employee("A1", "A2");
+var supervisor = new Supervisor("B1", "B2");
 
 while (true)
 {
-    Console.WriteLine("Podaj kolejną ocenę Pracownika: ");
+    Console.WriteLine("Type another grade of your Employee (A-E or 0-100)");
+    Console.WriteLine("or press 'z' to go to grading your Supervisor and get statistics");
     var input = Console.ReadLine();
-    if(input == "q")
+    if (input == "z")
     {
         break;
     }
-
     try
     {
         employee.AddGrade(input);
@@ -25,11 +26,40 @@ while (true)
     }
 }
 
-var statistics = employee.GetStatistics();
+while (true)
+{
+    Console.WriteLine("Type another grade of your Supervisor (1-6 including +/-)");
+    Console.WriteLine("or press 'q' to get statistics");
+    var input = Console.ReadLine();
+    if (input == "q")
+    {
+        break;
+    }
 
-Console.WriteLine("============================================");
-Console.WriteLine("Summary: ");
-Console.WriteLine($"--> Average: {statistics.Average}");
-Console.WriteLine($"--> Average grade as letter: {statistics.AverageLetter}");
-Console.WriteLine($"--> Min: {statistics.Min}");
-Console.WriteLine($"--> Max: {statistics.Max}");
+    try
+    {
+        supervisor.AddGrade(input);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Exception catched: {e.Message}");
+    }
+}
+
+var statisticsEmployee = employee.GetStatistics();
+var statisticsSupervisor = supervisor.GetStatistics();
+
+Console.WriteLine("======================================================");
+Console.WriteLine("Employee Summary: ");
+Console.WriteLine($"--> Average: {statisticsEmployee.Average}");
+Console.WriteLine($"--> Average grade as letter: {statisticsEmployee.AverageLetter}");
+Console.WriteLine($"--> Min: {statisticsEmployee.Min}");
+Console.WriteLine($"--> Max: {statisticsEmployee.Max}");
+
+Console.WriteLine();
+
+Console.WriteLine("Supervisor Summary: ");
+Console.WriteLine($"--> Average: {statisticsSupervisor.Average}");
+Console.WriteLine($"--> Average grade as letter: {statisticsSupervisor.AverageLetter}");
+Console.WriteLine($"--> Min: {statisticsSupervisor.Min}");
+Console.WriteLine($"--> Max: {statisticsSupervisor.Max}");
--- ChallengeApp/ChallengeApp/Supervisor.cs
+++ ChallengeApp/ChallengeApp/Supervisor.cs
@@ -1,16 +1,16 @@
 ﻿namespace ChallengeApp
 {
-    public class Employee : IEmployee
+    public class Supervisor : IEmployee
     {
         private List<float> grades = new List<float>();
 
-         public Employee(string name, string surname)
+        public Supervisor (string name, string surname)
         {
             this.Name = name;
             this.Surname = surname;
         }
-
         public string Name { get; private set; }
+
         public string Surname { get; private set; }
 
         public void AddGrade(float grade)
@@ -27,41 +27,70 @@
 
         public void AddGrade(string grade)
         {
-            if (float.TryParse(grade, out float result))
             {
-                this.AddGrade(result);
-            }
-            else if (char.TryParse(grade, out char charResult))
-            {
-                switch (charResult)
+                switch (grade)
                 {
-                    case 'A':
-                    case 'a':
-                        this.AddGrade(100);
+                    case "6":
+                        AddGrade(100);
                         break;
-                    case 'B':
-                    case 'b':
-                        this.AddGrade(80);
+                    case "-6":
+                    case "6-":
+                        AddGrade(95);
                         break;
-                    case 'C':
-                    case 'c':
-                        this.AddGrade(60);
+                    case "+5":
+                    case "5+":
+                        AddGrade(85);
                         break;
-                    case 'D':
-                    case 'd':
-                        this.AddGrade(40);
+                    case "5":
+                        AddGrade(80);
                         break;
-                    case 'E':
-                    case 'e':
-                        this.AddGrade(20);
+                    case "-5":
+                    case "5-":
+                        AddGrade(75);
+                        break;
+                    case "+4":
+                    case "4+":
+                        AddGrade(65);
+                        break;
+                    case "4":
+                        AddGrade(60);
+                        break;
+                    case "-4":
+                    case "4-":
+                        AddGrade(55);
+                        break;
+                    case "+3":
+                    case "3+":
+                        AddGrade(45);
+                        break;
+                    case "3":
+                        AddGrade(40);
+                        break;
+                    case "-3":
+                    case "3-":
+                        AddGrade(35);
+                        break;
+                    case "+2":
+                    case "2+":
+                        AddGrade(25);
+                        break;
+                    case "2":
+                        AddGrade(20);
+                        break;
+                    case "-2":
+                    case "2-":
+                        AddGrade(15);
+                        break;
+                    case "+1":
+                    case "1+":
+                        AddGrade(5);
+                        break;
+                    case "1":
+                        AddGrade(0);
                         break;
                     default:
-                        throw new Exception("Wrong Letter");
+                        throw new Exception("Wrong Figure. Type a grade between 1-6.");
                 }
-            }
-            else
-            {
-                throw new Exception("Type figure or letter between A - E");
             }
         }
 
@@ -78,7 +107,6 @@
             this.AddGrade(valueLong);
         }
 
-        //Przygotowanie metody, która zwróci oceny.
         public Statistics GetStatistics()
         {
             var statistics = new Statistics();
@@ -95,7 +123,7 @@
 
             statistics.Average /= this.grades.Count;
 
-            switch(statistics.Average)
+            switch (statistics.Average)
             {
                 case var average when average >= 80:
                     statistics.AverageLetter = 'A';
