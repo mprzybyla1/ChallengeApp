5ae8f0978b1b6904c16bb3c672b7387a4b53e4e7
Mateusz Przybyła <mateusz.piotr.przybyla@gmail.com>
2023-06-04 22:54:41 +0200

    Zadanie Dzień 16

---
+++ ChallengeApp/ChallengeApp/EmployeeInMemory.cs
@@ -0,0 +1,117 @@
+﻿namespace ChallengeApp
+{
+    public class EmployeeInMemory : EmployeeBase
+    {
+        private List<float> grades = new List<float>();
+
+        public EmployeeInMemory(string name, string surname) 
+            : base(name, surname)
+        {
+        }
+
+        public override void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                throw new Exception("Invalid grade value");
+            }
+        }
+
+        public override void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else if (char.TryParse(grade, out char charResult))
+            {
+                switch (charResult)
+                {
+                    case 'A':
+                    case 'a':
+                        this.AddGrade(100);
+                        break;
+                    case 'B':
+                    case 'b':
+                        this.AddGrade(80);
+                        break;
+                    case 'C':
+                    case 'c':
+                        this.AddGrade(60);
+                        break;
+                    case 'D':
+                    case 'd':
+                        this.AddGrade(40);
+                        break;
+                    case 'E':
+                    case 'e':
+                        this.AddGrade(20);
+                        break;
+                    default:
+                        throw new Exception("Wrong Letter");
+                }
+            }
+            else
+            {
+                throw new Exception("Type figure or letter between A - E");
+            }
+        }
+
+        public override void AddGrade(double grade)
+        {
+            grade = Math.Round(grade, 2);
+            float valueDouble = (float)grade;
+            this.AddGrade(valueDouble);
+        }
+
+        public override void AddGrade(long grade)
+        {
+            float valueLong = (float)grade;
+            this.AddGrade(valueLong);
+        }
+
+        public override Statistics GetStatistics()
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                if (grade > 0)
+                {
+                    statistics.Max = Math.Max(statistics.Max, grade);
+                    statistics.Min = Math.Min(statistics.Min, grade);
+                    statistics.Average += grade;
+                }
+            }
+
+            statistics.Average /= this.grades.Count;
+
+            switch (statistics.Average)
+            {
+                case var average when average >= 80:
+                    statistics.AverageLetter = 'A';
+                    break;
+                case var average when average >= 60:
+                    statistics.AverageLetter = 'B';
+                    break;
+                case var average when average >= 40:
+                    statistics.AverageLetter = 'C';
+                    break;
+                case var average when average >= 20:
+                    statistics.AverageLetter = 'D';
+                    break;
+                default:
+                    statistics.AverageLetter = 'E';
+                    break;
+            }
+            return statistics;
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -4,50 +4,55 @@ Console.WriteLine("Welcome to XYZ Program to collect grades of your Employees an
 Console.WriteLine("======================================================");
 Console.WriteLine();
 
-var employee = new Employee("A1", "A2");
-var supervisor = new Supervisor("B1", "B2");
+var employee = new EmployeeInFile("A", "B");
+employee.AddGrade(2.5f);
+employee.AddGrade(20);
+employee.AddGrade("B");
 
-while (true)
-{
-    Console.WriteLine("Type another grade of your Employee (A-E or 0-100)");
-    Console.WriteLine("or press 'z' to go to grading your Supervisor and get statistics");
-    var input = Console.ReadLine();
-    if (input == "z")
-    {
-        break;
-    }
-    try
-    {
-        employee.AddGrade(input);
-    }
-    catch(Exception e) 
-    {
-        Console.WriteLine($"Exception catched: {e.Message}");
-    }
-}
+//var employee = new Employee("A1", "A2");
+//var supervisor = new Supervisor("B1", "B2");
 
-while (true)
-{
-    Console.WriteLine("Type another grade of your Supervisor (1-6 including +/-)");
-    Console.WriteLine("or press 'q' to get statistics");
-    var input = Console.ReadLine();
-    if (input == "q")
-    {
-        break;
-    }
+//while (true)
+//{
+//    Console.WriteLine("Type another grade of your Employee (A-E or 0-100)");
+//    Console.WriteLine("or press 'z' to go to grading your Supervisor and get statistics");
+//    var input = Console.ReadLine();
+//    if (input == "z")
+//    {
+//        break;
+//    }
+//    try
+//    {
+//        employee.AddGrade(input);
+//    }
+//    catch (Exception e)
+//    {
+//        Console.WriteLine($"Exception catched: {e.Message}");
+//    }
+//}
 
-    try
-    {
-        supervisor.AddGrade(input);
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine($"Exception catched: {e.Message}");
-    }
-}
+//while (true)
+//{
+//    Console.WriteLine("Type another grade of your Supervisor (1-6 including +/-)");
+//    Console.WriteLine("or press 'q' to get statistics");
+//    var input = Console.ReadLine();
+//    if (input == "q")
+//    {
+//        break;
+//    }
+
+//    try
+//    {
+//        supervisor.AddGrade(input);
+//    }
+//    catch (Exception e)
+//    {
+//        Console.WriteLine($"Exception catched: {e.Message}");
+//    }
+//}
 
 var statisticsEmployee = employee.GetStatistics();
-var statisticsSupervisor = supervisor.GetStatistics();
+// var statisticsSupervisor = supervisor.GetStatistics();
 
 Console.WriteLine("======================================================");
 Console.WriteLine("Employee Summary: ");
@@ -56,10 +61,10 @@ Console.WriteLine($"--> Average grade as letter: {statisticsEmployee.AverageLett
 Console.WriteLine($"--> Min: {statisticsEmployee.Min}");
 Console.WriteLine($"--> Max: {statisticsEmployee.Max}");
 
-Console.WriteLine();
+//Console.WriteLine();
 
-Console.WriteLine("Supervisor Summary: ");
-Console.WriteLine($"--> Average: {statisticsSupervisor.Average}");
-Console.WriteLine($"--> Average grade as letter: {statisticsSupervisor.AverageLetter}");
-Console.WriteLine($"--> Min: {statisticsSupervisor.Min}");
-Console.WriteLine($"--> Max: {statisticsSupervisor.Max}");
+//Console.WriteLine("Supervisor Summary: ");
+//Console.WriteLine($"--> Average: {statisticsSupervisor.Average}");
+//Console.WriteLine($"--> Average grade as letter: {statisticsSupervisor.AverageLetter}");
+//Console.WriteLine($"--> Min: {statisticsSupervisor.Min}");
+//Console.WriteLine($"--> Max: {statisticsSupervisor.Max}");
